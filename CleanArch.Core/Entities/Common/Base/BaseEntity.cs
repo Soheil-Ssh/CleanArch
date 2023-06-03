a41b355875d1ba78af48ce0ed8ad09ea10e568d5
Soheil-Ssh <soheil.ssh1280@gmail.com>
2023-06-04 02:52:36 +0330

    Add base entity

---
+++ CleanArch.Core/Entities/Common/Base/BaseEntity.cs
@@ -0,0 +1,18 @@
+﻿namespace CleanArch.Core.Entities.Common.Base
+{
+    public class BaseEntity
+    {
+        #region Properties
+
+        [Key]
+        public Guid Id { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+        public DateTime? LastUpdateDate { get; set; }
+
+        public bool IsDelete { get; set; }
+
+        #endregion
+    }
+}
