50d06509ee90da63f70f3bb6c9fded46e5c7de9a
Soheil-Ssh <soheil.ssh1280@gmail.com>
2023-06-04 02:56:13 +0330

    Add person model

---
+++ CleanArch.Core/Entities/Person/Person.cs
@@ -0,0 +1,32 @@
+﻿namespace CleanArch.Core.Entities.Person
+{
+    public class Person : BaseEntity
+    {
+        #region Properties
+
+        [Required]
+        [MaxLength(200)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string LastName { get; set; }
+
+        [Required]
+        [MaxLength(500)]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [Range(0, Int32.MaxValue)]
+        public int Age { get; set; }
+
+        [MaxLength(200)]
+        public string Jub { get; set; }
+
+        #endregion
+    }
+}
