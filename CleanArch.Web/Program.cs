e7b5be3948dc2d4c17e2ba6dd46546b16e8fd798
Soheil-Ssh <soheil.ssh1280@gmail.com>
2023-06-04 03:49:30 +0330

    Initial web layer

--- CleanArch.Web/Program.cs
+++ CleanArch.Web/Program.cs
@@ -1,15 +1,38 @@
-var builder = WebApplication.CreateBuilder(args);
+using CleanArch.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 
-// Add services to the container.
-builder.Services.AddRazorPages();
+#region Services
+
+
+var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+var configuration = builder.Configuration;
+
+#region Db context
+
+services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(
+        configuration.GetConnectionString(
+            StaticDataStore.SqlServerConnectionStringName)));
+
+#endregion
+
+#region Mvc
+
+services.AddControllersWithViews()
+    .AddRazorRuntimeCompilation();
+
+#endregion
+
+#endregion
+
+#region Middlewares
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -20,6 +43,13 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapRazorPages();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+});
 
 app.Run();
+
+#endregion
