0ffafca9c5243a944c758316d7619aea762701c5
Soheil-Ssh <soheil.ssh1280@gmail.com>
2023-06-04 03:40:12 +0330

    Add static data store

---
+++ CleanArch.Common/Stores/StaticDataStore.cs
@@ -0,0 +1,11 @@
+﻿namespace CleanArch.Common.Stores
+{
+    public static class StaticDataStore
+    {
+        #region Connection
+
+        public const string SqlServerConnectionStringName = "DefaultConnection";
+
+        #endregion
+    }
+}
