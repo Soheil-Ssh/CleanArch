b25e3995651829e3c7cee47e4eb60536a691784e
Soheil-Ssh <soheil.ssh1280@gmail.com>
2023-06-07 02:54:47 +0330

    Initial IoC layer

---
+++ CleanArch.IoC/DependenciesContainers/ApplicationDependenciesContainer.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArch.IoC.DependenciesContainers
+{
+    public static class ApplicationDependenciesContainer
+    {
+        public static IServiceCollection AddApplicationDependencies(this IServiceCollection services)
+        {
+
+            return services;
+        }
+    }
+}
