5dd5df0de58892f16191bc0639f34aa51ad04898
Soheil-Ssh <soheil.ssh1280@gmail.com>
2023-06-10 04:43:26 +0330

    Add IsDelete filter

--- CleanArch.Infrastructure/Data/ApplicationDbContext.cs
+++ CleanArch.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
 
 namespace CleanArch.Infrastructure.Data
 {
@@ -15,6 +17,28 @@ namespace CleanArch.Infrastructure.Data
         #region DbSets
 
         public DbSet<Person> People { get; set; }
+
+        #endregion
+
+        #region Model creating
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            Expression<Func<BaseEntity, bool>> filterExpr = be => !be.IsDelete;
+            foreach (var mutableEntityType in builder.Model.GetEntityTypes())
+            {
+                if (mutableEntityType.ClrType.IsAssignableTo(typeof(BaseEntity)))
+                {
+                    var parameter = Expression.Parameter(mutableEntityType.ClrType);
+                    var body = ReplacingExpressionVisitor.Replace(filterExpr.Parameters.First(), parameter, filterExpr.Body);
+                    var lambdaExpression = Expression.Lambda(body, parameter);
+
+                    mutableEntityType.SetQueryFilter(lambdaExpression);
+                }
+            }
+        }
 
         #endregion
     }
