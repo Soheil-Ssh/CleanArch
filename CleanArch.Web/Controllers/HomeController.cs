683267e5a78142694635e5c5aa337deabbf9cebe
Soheil-Ssh <soheil.ssh1280@gmail.com>
2023-06-10 04:06:37 +0330

    Add intialy view and home controller

---
+++ CleanArch.Web/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArch.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        #region Index
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #endregion
+    }
+}
