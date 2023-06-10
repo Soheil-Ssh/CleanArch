589b5856ba276e3c5da7937a67cc34a69815472a
Soheil-Ssh <soheil.ssh1280@gmail.com>
2023-06-10 04:46:36 +0330

    Add intialy people controller

---
+++ CleanArch.Web/Controllers/PeopleController.cs
@@ -0,0 +1,28 @@
+﻿using CleanArch.Application.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArch.Web.Controllers
+{
+    public class PeopleController : Controller
+    {
+        #region Ctor
+
+        private readonly IPeopleService _peopleService;
+        private readonly ILogger<PeopleController> _logger;
+
+        public PeopleController(IPeopleService peopleService,
+            ILogger<PeopleController> logger)
+        {
+            _peopleService = peopleService;
+            _logger = logger;
+        }
+
+        #endregion
+
+        #region Index
+
+        public IActionResult Index() => View();
+
+        #endregion
+    }
+}
