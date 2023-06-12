0252433498ed9249784eaa52d9cec133bb53d57d
Soheil-Ssh <soheil.ssh1280@gmail.com>
2023-06-12 23:38:42 +0330

    Add json result

---
+++ CleanArch.Core/Entities/Common/Enums/JsonResultType.cs
@@ -0,0 +1,16 @@
+﻿namespace CleanArch.Core.Entities.Common.Enums
+{
+    public enum JsonResultType
+    {
+        CreateSucceeded,
+        CreateFailed,
+        EditSucceeded,
+        EditFailed,
+        DeleteSucceeded,
+        DeleteFailed,
+        Error,
+        ValidationError,
+        NotFoundError,
+        ImageSecurityError,
+    }
+}
---
+++ CleanArch.Core/Entities/Common/Result/JsonResult.cs
@@ -0,0 +1,15 @@
+﻿namespace CleanArch.Core.Entities.Common.Result
+{
+    public class JsonResult
+    {
+        #region properties
+
+        public bool Succeeded { get; set; }
+
+        public string Message { get; set; }
+
+        public object Data { get; set; }
+
+        #endregion
+    }
+}
