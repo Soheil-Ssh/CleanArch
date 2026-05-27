4979e5bdf48481d33d90aa2ea628adf3b87869ee
Soheil Shahsavan <soheil.ssh1280@gmail.com>
2026-05-28 02:46:53 +0330

    feat: add application and infra dependencies

---
+++ Src/CleanArch.Application/DependenciesContainer.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArch.Application;
+
+public static class DependenciesContainer
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        // add MediatR
+        services.AddMediatR(options =>
+            options.RegisterServicesFromAssemblyContaining(typeof(DependenciesContainer)));
+
+        // add Auto mapper
+        services.AddAutoMapper(cfg => { }, typeof(DependenciesContainer).Assembly);
+
+        // add Fluent validation
+        services.AddValidatorsFromAssembly(typeof(DependenciesContainer).Assembly);
+
+        return services;
+    }
+}
---
+++ Src/CleanArch.Infrastructure/DependenciesContainer.cs
@@ -0,0 +1,22 @@
+﻿using CleanArch.Core.IRepositories.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArch.Infrastructure;
+
+public static class DependenciesContainer
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        // Add context
+        services.AddDbContext<ApplicationDbContext>(options
+            => options.UseSqlServer(configuration
+                .GetConnectionString("DefaultConnection")));
+
+        // Add unit of work
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+        return services;
+    }
+}
