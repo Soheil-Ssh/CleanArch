e3252f2ec0dd79e6c12c85cfecf9045d4aff3e09
Soheil Shahsavan <soheil.ssh1280@gmail.com>
2026-05-27 23:14:46 +0330

    feat: application db context

---
+++ Src/CleanArch.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using CleanArch.Core.Entities.ToDo;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArch.Infrastructure.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    public DbSet<ToDo> ToDos { get; set; }
+}
